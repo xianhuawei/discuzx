618545c0779eae1685cd1fdab1057d0c51a93616
= <=>
2014-01-19 17:43:49 +0800

    add xiaomi template

---
+++ template/xiaomi/img/js/jquery.sgallery.js
@@ -0,0 +1,206 @@
+﻿/*
+ *	sGallery 1.0 - simple gallery with jQuery
+ *	made by bujichong 2009-11-25
+ *	作者：不羁虫  2009-11-25
+ * http://hi.baidu.com/bujichong/
+ *	欢迎交流转载，但请尊重作者劳动成果，标明插件来源及作者
+ */
+
+(function (jQuery) {
+jQuery.fn.sGallery = function (o) {
+    return  new jQuerysG(this, o);
+			//alert('do');
+    };
+
+	var settings = {
+		thumbObj:null,//预览对象
+		titleObj:null,//标题
+		botLast:null,//按钮上一个
+		botNext:null,//按钮下一个
+		thumbNowClass:'now',//预览对象当前的class,默认为now
+		slideTime:800,//平滑过渡时间
+		autoChange:true,//是否自动切换
+		changeTime:5000,//自动切换时间
+		delayTime:100//鼠标经过时反应的延迟时间
+	};
+
+ jQuery.sGalleryLong = function(e, o) {
+    this.options = jQuery.extend({}, settings, o || {});
+	var _self = jQuery(e);
+	var set = this.options;
+	var thumb;
+	var size = _self.size();
+	var nowIndex = 0; //定义全局指针
+	var index;//定义全局指针
+	var startRun;//预定义自动运行参数
+	var delayRun;//预定义延迟运行参数
+
+//初始化
+	_self.eq(0).show();
+
+//主切换函数
+function fadeAB () {
+	if (nowIndex != index) {
+		if (set.thumbObj!=null) {
+		jQuery(set.thumbObj).removeClass().eq(index).addClass(set.thumbNowClass);}
+		_self.eq(nowIndex).hide();
+		_self.eq(index).stop(true,true).fadeIn(set.slideTime);
+		jQuery(set.titleObj).eq(nowIndex).hide();//新增加title
+		jQuery(set.titleObj).eq(index).show();//新增加title
+		nowIndex = index;
+		if (set.autoChange==true) {
+		clearInterval(startRun);//重置自动切换函数
+		startRun = setInterval(runNext,set.changeTime);}
+		}
+}
+
+//切换到下一个
+function runNext() {
+	index =  (nowIndex+1)%size;
+	fadeAB();
+}
+
+//点击任一图片
+	if (set.thumbObj!=null) {
+	thumb = jQuery(set.thumbObj);
+//初始化
+	thumb.eq(0).addClass(set.thumbNowClass);
+		thumb.bind("mousemove",function(event){
+			index = thumb.index(jQuery(this));
+			fadeAB();
+			delayRun = setTimeout(fadeAB,set.delayTime);
+			clearTimeout(delayRun);
+			event.stopPropagation();
+		})
+	}
+
+//点击上一个
+	if (set.botNext!=null) {
+		var botNext = jQuery(set.botNext);
+		botNext.mousemove(function () {
+			runNext();
+			return false;
+		});
+	}
+
+//点击下一个
+	if (set.botLast!=null) {
+		var botLast = jQuery(set.botLast);
+		botLast.mousemove(function () {
+			index = (nowIndex+size-1)%size;
+			fadeAB();
+			return false;
+	});
+	}
+
+//自动运行
+	if (set.autoChange==true) {
+	startRun = setInterval(runNext,set.changeTime);
+	}
+
+}
+
+var jQuerysG = jQuery.sGalleryLong;
+
+})(jQuery);
+
+function slide(Name,Class,Width,Height,fun){
+	jQuery(Name).width(Width);
+	jQuery(Name).height(Height);
+	
+	if(fun == true){
+		jQuery(Name).append('<div class="title-bg"></div><div class="title"></div><div class="change"></div>')
+		var atr = jQuery(Name+' div.changeDiv a');
+		var sum = atr.length;
+		for(i=1;i<=sum;i++){
+			var title = atr.eq(i-1).attr("title");
+			var href = atr.eq(i-1).attr("href");
+			jQuery(Name+' .change').append('<i>'+i+'</i>');
+			jQuery(Name+' .title').append('<a href="'+href+'">'+title+'</a>');
+		}
+		jQuery(Name+' .change i').eq(0).addClass('cur');
+	}
+	jQuery(Name+' div.changeDiv a').sGallery({//对象指向层，层内包含图片及标题
+		titleObj:Name+' div.title a',
+		thumbObj:Name+' .change i',
+		thumbNowClass:Class
+	});
+	jQuery(Name+" .title-bg").width(Width);
+}
+
+//缩略图
+jQuery.fn.LoadImage=function(scaling,width,height,loadpic){
+    if(loadpic==null)loadpic="../images/msg_img/loading.gif";
+return this.each(function(){
+   var t=jQuery(this);
+   var src=jQuery(this).attr("src")
+   var img=new Image();
+   img.src=src;
+   //自动缩放图片
+   var autoScaling=function(){
+    if(scaling){
+     if(img.width>0 && img.height>0){ 
+           if(img.width/img.height>=width/height){ 
+               if(img.width>width){ 
+                   t.width(width); 
+                   t.height((img.height*width)/img.width); 
+               }else{ 
+                   t.width(img.width); 
+                   t.height(img.height); 
+               } 
+           } 
+           else{ 
+               if(img.height>height){ 
+                   t.height(height); 
+                   t.width((img.width*height)/img.height); 
+               }else{ 
+                   t.width(img.width); 
+                   t.height(img.height); 
+               } 
+           } 
+       } 
+    } 
+   }
+   //处理ff下会自动读取缓存图片
+   if(img.complete){
+    //autoScaling();
+      return;
+   }
+   jQuery(this).attr("src","");
+   var loading=jQuery("<img alt=\"加载中...\" title=\"图片加载中...\" src=\""+loadpic+"\" />");
+  
+   t.hide();
+   t.after(loading);
+   jQuery(img).load(function(){
+    //autoScaling();
+    loading.remove();
+    t.attr("src",this.src);
+    t.show();
+	//jQuery('.photo_prev a,.photo_next a').height(jQuery('#big-pic img').height());
+   });
+  });
+}
+
+//向上滚动代码
+function startmarquee(elementID,h,n,speed,delay){
+ var t = null;
+ var box = '#' + elementID;
+ jQuery(box).hover(function(){
+  clearInterval(t);
+  }, function(){
+  t = setInterval(start,delay);
+ }).trigger('mouseout');
+ function start(){
+  jQuery(box).children('ul:first').animate({marginTop: '-='+h},speed,function(){
+   jQuery(this).css({marginTop:'0'}).find('li').slice(0,n).appendTo(this);
+  })
+ }
+}
+
+//TAB切换
+function SwapTab(name,title,content,Sub,cur){
+  jQuery(name+' '+title).mouseover(function(){
+	  jQuery(this).addClass(cur).siblings().removeClass(cur);
+	  jQuery(content+" > "+Sub).eq(jQuery(name+' '+title).index(this)).show().siblings().hide();
+  });
+}
---
+++ template/xiaomi/img/js/ssp_effectPage.js
@@ -0,0 +1,205 @@
+﻿/* ***************************************
+name: advertisementSwitch
+import: jquery1.4
+***************************************** */
+var effectPage = function (slider, options) {
+    // 类变量
+    
+    this.list = jQuery(slider); // 列表
+	this.slider = this.list.parent(); // 容器
+    this.count = this.list.size(); // 合计
+    this.index = 0; //数量
+    this.prevIndex = 0; // 上一索引
+    this.timer = null; // 计时器
+	this.allowShow = true; // 切换频率显示
+	this.playType  = "Next"; // 播放类型
+
+
+    // 选项
+    this.options = {//默认值
+        Auto: true, //是否自动
+        Duration: 800, //持续时间
+        Time: 1000, //延时
+        Pause: 3000, //停顿时间(Auto为true时有效),
+        effect: "", //播放效果
+        pageBind: "", // 切换绑定
+		lazyLoad: false, // 延迟加载
+		useMousePage: false, // 使用鼠标点击翻页
+        pageSelectClass: "" //选中样式
+    };
+
+   
+
+    jQuery.extend(this.options, options || {});
+    //效果方法名
+    this.effectName = this.options.effect; //效果名   
+	if ( !this.options.autoSize) {// 父级自适应图片大小
+		if ( this.options.useMousePage ) this.useMousePage();     //鼠标翻页
+	};
+	if ( this.options.keyboardPage ) this.useKeyboardPage();   //键盘翻页
+};
+
+effectPage.prototype = {
+    PlayEffect: function () { // 效果切换
+        switch (this.effectName) {
+            case "fade":
+                this.fadeInOut();
+                break; 
+            default:
+                this.InOut();
+                break;
+        };
+    },
+    InOut: function () {//默认
+        var index = this.index;
+        if (this.list.length > 0 && index != this.prevIndex) {
+            jQuery(this.list[index]).css({ "z-index": "1" }).show();
+            jQuery(this.list[this.prevIndex]).css({ "z-index": "0" }).hide();
+        }
+    },
+    fadeInOut: function () {//淡入淡出
+        var index = this.index;
+        if (this.list.length > 0 && index != this.prevIndex) {
+            jQuery(this.list[index]).css({ "z-index": "1" }).fadeIn(this.options.Duration);
+            jQuery(this.list[this.prevIndex]).css({ "z-index": "0" }).fadeOut(this.options.Duration); ;
+        }
+    },
+    PageStyleSwitch: function () {
+        var index = this.index;
+        if (this.options.pageSelectClass.length > 0) {
+            var pageList = jQuery("#" + this.options.pageBind).children();
+            jQuery(pageList[this.index]).addClass(this.options.pageSelectClass);
+            if (this.index != this.prevIndex) jQuery(pageList[this.prevIndex]).removeClass(this.options.pageSelectClass);  
+        }
+    },
+    // 继承绑定
+    Bind: function (object, fun) {
+        var args = Array.prototype.slice.call(arguments).slice(2);
+        return function () {
+            return fun.apply(object, args.concat(Array.prototype.slice.call(arguments)));
+        };
+    },
+	// 使用鼠标点击翻页
+	useMousePage: function(){
+		var that = this;
+		// 获取父容器高度/宽度
+		var parentWidth  = this.slider.width(), 
+			parentHeight = this.slider.height();
+		var divStyle = "position: absolute; display: block; opacity: 0; filter: Alpha(opacity=0)\9; top: 0; background: #ffffff; z-index: 9;"
+		             + "width: " + parentWidth/2 + "px;" + "height: " + parentHeight + "px;";
+					 
+
+		// 建立翻转触发
+		var leftDiv = document.createElement("DIV");
+		leftDiv.style.cssText = divStyle + "left: 0";
+		leftDiv.onclick =  function(){
+			that.Prev();
+		};
+		this.slider.append(leftDiv);
+
+		var rightDiv = document.createElement("DIV");
+		rightDiv.style.cssText = divStyle + "left: " + parentWidth/2 + "px;";
+		rightDiv.onclick = function(){
+			that.Next();
+		};
+		this.slider.append(rightDiv);
+	},
+	// 使用键盘翻页
+	useKeyboardPage: function(){
+		var keyControl = this.options.keyboardPage || "",  keyString = ["updown","leftright"], that = this;
+		keyControl = keyControl.toString().toLocaleLowerCase();
+		if (keyControl.length === 0 || jQuery.inArray(keyControl, keyString) < 0) return;
+		
+		jQuery(document).keydown(function(e){
+			var keyCode = keyControl === "updown" ? [38, 40] : [37, 39];
+			if (e.keyCode === keyCode[0]) that.Prev();
+			if (e.keyCode === keyCode[1]) that.Next();
+		});
+
+	},
+	// 频率控制
+	switchSpeed: function(){
+		clearTimeout(this.allowShowTimer);
+		this.allowShowTimer = null;
+		this.allowShow = true;
+		this.Run(this.index);
+	},
+	// 加载图片
+	LoadImage: function( imgUrl, index ){
+		var that = this,
+			imgNew = new Image(),
+			imgControl = that.list[index];
+		imgControl.removeAttribute("lazysrc");
+		imgNew.src	   	 = imgUrl;
+		imgNew.index     = index;
+		imgControl.src   = imgUrl;
+		imgNew.onload = function () {
+			that.Play(imgNew.index);
+			that.allowShow = true;
+			imgNew.onload = null;
+			jQuery(".loading").css({"display": "none"});
+		};
+	},
+    // 设置变量
+    Run: function (index, t) {
+    	this.playType = t;
+        clearTimeout(this.timer);
+        //修正index
+        index == undefined && (index = this.index);
+        index < 0 && (index = this.count - 1) || index >= this.count && (index = 0);
+        this.index = index;
+		var imgControl = this.list[index],
+			imgUrl     = imgControl.getAttribute("lazysrc");
+
+		if ( this.options.lazyLoad ){
+			if (imgUrl){
+				// 控制切换频率
+				if(!this.allowShow) return;
+				jQuery(".loading").css({"display": "block"});
+				this.allowShow = false;
+				this.LoadImage(imgUrl, index);
+				
+			}else{
+				this.Play(index);	
+			};
+
+		}else{
+			this.Play(index); //播放
+		};
+		
+    },
+	// Play
+	Play: function (index){
+		// ++pv
+		// this.cnzzCount();
+		this.index = index;
+		this.PlayEffect(); //切换播放效果
+        this.PageStyleSwitch(); //选择样式切换
+        this.prevIndex = index;
+        // 外部翻页触发事件
+        if (this.options.flipPage) this.options.flipPage(index, this.playType);
+        // 自动播放
+        this.options.Auto && (this.timer = setTimeout(this.Bind(this, this.Next), this.options.Pause));
+	},
+	// cnzz count
+	// cnzzCount: function(){
+	// 	var imgV    = jQuery("img[src*='cnzz.com']"),
+	// 		imgN    = document.createElement("IMG");
+	// 	imgN.width  = 0;
+	// 	imgN.height = 0;
+	// 	imgN.border = 0;
+	// 	imgN.src    = imgV.attr("src");
+	// 	//delete
+	// 	imgV.remove();
+	// 	//create
+	// 	document.body.appendChild(imgN);
+
+	// },
+	First: function (){ this.Run(0, "First"); },
+	End : function () { this.Run(this.count-1, "End"); },
+    Next: function () { this.Run(++this.index, "Next");  },
+    Prev: function () { this.Run(--this.index, "Prev"); },
+    Stop: function () { }
+};
+
+
