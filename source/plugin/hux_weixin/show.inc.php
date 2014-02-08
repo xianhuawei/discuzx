d288999b36db23be39e60292fc751684d4c209ca
= <=>
2014-02-09 07:07:08 +0800

    remove some plugin

--- source/plugin/hux_weixin/show.inc.php
+++
@@ -1,9 +0,0 @@
-<?php
-if(!defined('IN_DISCUZ')) {
-	exit('Access Denied');
-}
-
-$wxsetting = $_G['cache']['plugin']['hux_weixin'];
-$zllist = "<br>".str_replace("\n","<br>",lang('plugin/hux_weixin','list'))."<br>help:".lang('plugin/hux_weixin','help');
-include template('hux_weixin:index');
-?>
