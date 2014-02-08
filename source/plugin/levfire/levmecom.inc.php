232e1d98af00ec9ac0e5faeabaa0487a29c69b06
= <=>
2014-02-09 04:28:52 +0800

    remove some plugin

--- source/plugin/levfire/levmecom.inc.php
+++
@@ -1,6 +0,0 @@
-<?php
-if(!defined('IN_DISCUZ') || !defined('IN_ADMINCP')){
-	exit('Access Denied');
-}
-
-header('location:http://addon.discuz.com/?@levmecom');
--- source/plugin/nimba_newuser/addon.inc.php
+++
@@ -1,5 +0,0 @@
-<?php
-if(!defined('IN_DISCUZ')) {
-	exit('Access Denied');
-}
-?>
