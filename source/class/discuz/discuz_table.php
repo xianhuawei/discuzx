e0aaa18a77798f6faadc261b0d4ef88ac922a076
= <=>
2014-02-09 01:17:08 +0800

    fix discuz_table.php

--- source/class/discuz/discuz_table.php
+++ source/class/discuz/discuz_table.php
@@ -31,7 +31,7 @@ class discuz_table extends discuz_base
 			$this->_pk = $para['pk'];
 		}
 		//如果 $this->_table 和 $this->_pk 不为空 则统一加上缓存
-		if(!isset($this->_pre_cache_key) && !empty($this->_table) && $this->_pk){
+		if(!isset($this->_pre_cache_key) && !empty($this->_table) && !empty($this->_pk)){
 			$this->_pre_cache_key = $this->_table.'_';
 			if($ttl = getglobal('setting/memory/'.$this->_table) == null){
 				$this->_cache_ttl = 86400;//默认缓存一天
